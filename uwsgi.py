19e88646e974cd613547d6827ff0072bf9ace90b
Wizzerinus <development@toontowneventhorizon.com>
2022-11-18 14:25:20 +0300

    attempt to setup uwsgi

---
+++ uwsgi.py
@@ -0,0 +1,9 @@
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+from shared.helpers.logging import initlogger
+from shared.helpers.util2 import update_distributions
+from website import app
+
+initlogger()
+update_distributions()
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
